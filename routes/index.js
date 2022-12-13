3222986e6ebf59e129f0c5ac9f7f17a1b12e5a11
Josech03 <whitesox3734@gmail.com>
2022-12-13 09:54:01 -0400

    commit

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var cookieParser= require('cookie-parser');
+var session = require('express-session');
+var PassportLocal = require('passport-local').Strategy;
+
+router.use(express.urlencoded({extended: true}));
+router.use(cookieParser("Un secreto"));
+router.use(session({
+	secret: "Un secreto",
+	resave: true,
+	saveUninitialized: true
+}));
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.use( new PassportLocal(function(username, password, done){
+
+	if(username === "holasoyadmin@gmail.com" && password === "holasoyadmin123")
+		return done(null,{id: 1, name: "Administrador"});
+
+	done(null, false)
+}))
+
+passport.serializeUser(function(user, done){
+	done(null, user.id)
+})
+
+passport.deserializeUser(function(_user, done){
+	done(null,{id: 1, name: "Aministrador"});
+});
+router.get('/login',(_req,res)=>{
+	res.render('login.ejs')
+});
+
+router.post('/login', passport.authenticate('local',{
+	successRedirect: "/",
+	failureRedirect: "/login"
+}));
+
+router.get('/',(req, res, next)=>{
+	if(req.isAuthenticated()) return next();
+
+	res.redirect("/login")
+})
+
+router.get('/', function(_req, res, _next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
